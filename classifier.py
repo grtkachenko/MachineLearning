c0ce89eeaf3b49080fead953b5ce681f0eb71248
Anton Kovsharov <antonkovsharov@gmail.com>
2014-09-21 18:09:02 +0400

    Added interface of classifier

---
+++ classifier.py
@@ -0,0 +1,11 @@
+class Classifier:
+    def __init__(self, k, train_data, metrics):
+        self.k = k
+        self.train_data = train_data
+        self.metrics = metrics
+
+    def get_class(self, test_point):
+        self.train_data.sorted(key=lambda point : self.metrics(point, test_point))
+        nearest_list = self.train_data[0:self.k]
+        return nearest_list[0].class_id
+
