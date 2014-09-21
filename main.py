70c46d8b7f5e55abcf80ca29e585ea8bef84e9db
Anton Kovsharov <antonkovsharov@gmail.com>
2014-09-21 20:39:04 +0400

    Fixed normalize

--- main.py
+++ main.py
@@ -15,22 +15,20 @@ def optimal_k(chips):
 
 
 def normalize(chips, coord_id):
-    max_chip = max(chips, key=lambda p: abs(p.get(coord_id)))
+    max_chip = max(chips, key=lambda p: abs(p.get(coord_id))).get(coord_id)
 
-    def f(chip):
+    def chip_fun(chip):
         if coord_id == 0:
             chip.x /= max_chip
         else:
             chip.y /= max_chip
         return chip
-    map(f, chips)
-
+    return list(map(chip_fun, chips))
 
 with open('chips.txt', 'r') as f:
     chips = [Data(*line.split(',')) for line in f.readlines()]
-    normalize(chips, 0)
-    normalize(chips, 1)
-    shuffle(chips)
+    chips = normalize(normalize(chips, 0), 1)
+        shuffle(chips)
     count_to_test = len(chips)//5
     test = chips[:count_to_test]
     chips = chips[count_to_test:]
