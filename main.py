dc98cc5da97c5a2e8e56c04b62e5be6e19ead4b2
Anton Kovsharov <antonkovsharov@gmail.com>
2014-09-21 18:09:19 +0400

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from data import Data
+from random import shuffle
+
+def optimal_k(chips):
+    return 2
 
 with open('chips.txt', 'r') as f:
     chips = [Data(*line.split(',')) for line in f.readlines()]
-    for chip in chips:
-        print(chip)
+    shuffle(chips)
+    count_to_test = len(chips)/5
+    test = chips[:count_to_test]
+    chips = chips[:count_to_test]
+    k = optimal_k(chips)
+    classifier = Classifier(k, chips)
+    
---
+++ metrics.py
@@ -0,0 +1,4 @@
+from math import sqrt
+
+def euclidean(p0, p1):
+    return sqrt((p0.x - p1.x)**2 + (p0.y - p1.y)**2)
