3894e94afc9474d9bcc32d9e6f10ae71e569886e
Anton Kovsharov <antonkovsharov@gmail.com>
2014-09-21 17:58:37 +0400

    dfds

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
-class data:
+class Data:
+    def __init__(self, x, y, class_id):
+        self.x = x
+        self.y = y
+        self.class_id = class_id
+
+    def __str__(self):
+        return "[{} {}] - {}".format(self.x, self.y, self.class_id)
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+from data import Data
 
-
+with open('chips.txt', 'r') as f:
+    chips = [Data(*line.split(',')) for line in f.readlines()]
+    for chip in chips:
+        print(chip)
