51ea20305704f9adf3205d4e7b94b960805aac53
Anton Kovsharov <antonkovsharov@gmail.com>
2014-09-28 17:41:33 +0400

    refactoring

--- Bayes/bayes_classifier.py
+++ Bayes/bayes_classifier.py
@@ -4,7 +4,7 @@ class BayesClassifier:
     def __init__(self, train_data):
         self.train_data = train_data
         words_cnt = dict()
-        class_size = [0, 0]
+        class_size = [0., 0.]
         for letter in train_data:
             class_size[letter.class_id] += 1
             for word in letter.subject + letter.body:
@@ -19,7 +19,8 @@ class BayesClassifier:
     def get_class(self, test_letter):
         cur_prob_classes = [self.prob_classes[0], self.prob_classes[1]]
         for word in test_letter.subject + test_letter.body:
-            for i in range(0, 2):
-                cur_prob_classes[i] *= self.word_prob[word][i]
+            for i in range(2):
+                if word in self.word_prob:
+                    cur_prob_classes[i] *= self.word_prob[word][i]
 
         return 0 if cur_prob_classes[0] > cur_prob_classes[1] else 1
