a73d558aa1fdda5b36cb46e150df12f449acd9cb
Grigory Tkachenko <grtkachenko@gmail.com>
2014-10-20 02:05:01 +0400

    Refactoring in smo.py

--- LinearClassifier/smo.py
+++ LinearClassifier/smo.py
@@ -1,71 +1,77 @@
-import numpy as np
-from common.kernels import dot
 from random import randint
 
+from common.kernels import dot
 
-def smo(C, tol, max_passes, pts, kernel=dot):
-    a = [0] * len(pts)
-    b = 0
-    passes = 0
-    while passes < max_passes:
-        num_changes_alpha = 0
-        for i in range(len(pts)):
-            cur_p = pts[i]
-            err_i = calc_e(cur_p, a, b, pts, kernel)
-            if (cur_p.class_id * err_i < -tol and a[i] < C) or (cur_p.class_id * err_i > tol and a[i] > 0):
-                j = i
-                while i == j:
-                    j = randint(0, len(pts) - 1)
-                err_j = calc_e(pts[j], a, b, pts, kernel)
-                a_old_i, a_old_j = a[i], a[j]
-                l, h = calc_lh(i, j, pts, a, C)
-                if l == h:
-                    continue
-                n = calc_n(pts[i], pts[j], kernel)
-                if n >= 0:
-                    continue
 
-                a[j] -= pts[j].class_id * (err_i - err_j) / n
-                a[j] = min(h, max(l, a[j]))
-                if abs(a[j] - a_old_j) < 1e-5:
-                    continue
-                a[i] += pts[i].class_id * pts[j].class_id * (a_old_j -  a[j])
-                b_help = b - pts[i].class_id * (a[i] - a_old_i) * kernel(pts[i], pts[i]) - pts[j].class_id * (a[j] - a_old_j) * kernel(pts[j], pts[j])
-                b1, b2 = b_help - err_i, b_help - err_j
-                if 0 < a[i] < C:
-                    b = b1
-                elif 0 < a[j] < C:
-                    b = b2
-                else:
-                    b = (b1 + b2) / 2
-                num_changes_alpha += 1
+class Smo:
+    def __init__(self, c, tol, max_passes, kernel=dot):
+        self.c = c
+        self.tol = tol
+        self.max_passes = max_passes
+        self.kernel = kernel
+    
+    def minimize(self, pts):
+        for p in pts:
+            p.class_id = -1 if p.class_id == 0 else 1
+        a = [0] * len(pts)
+        b = 0
+        passes = 0
+        while passes < self.max_passes:
+            num_changes_alpha = 0
+            for i in range(len(pts)):
+                cur_p = pts[i]
+                err_i = self.calc_e(cur_p, a, b, pts)
+                if (cur_p.class_id * err_i < -self.tol and a[i] < self.c) or (cur_p.class_id * err_i > self.tol and a[i] > 0):
+                    j = i
+                    while i == j:
+                        j = randint(0, len(pts) - 1)
+                    err_j = self.calc_e(pts[j], a, b, pts)
+                    a_old_i, a_old_j = a[i], a[j]
+                    l, h = self.calc_lh(i, j, pts, a)
+                    if l == h:
+                        continue
+                    n = self.calc_n(pts[i], pts[j])
+                    if n >= 0:
+                        continue
+    
+                    a[j] -= pts[j].class_id * (err_i - err_j) / n
+                    a[j] = min(h, max(l, a[j]))
+                    if abs(a[j] - a_old_j) < 1e-5:
+                        continue
+                    a[i] += pts[i].class_id * pts[j].class_id * (a_old_j - a[j])
+                    b_help = b - pts[i].class_id * (a[i] - a_old_i) * self.kernel(pts[i], pts[i]) - pts[j].class_id * (a[j] - a_old_j) * self.kernel(pts[j], pts[j])
+                    b1, b2 = b_help - err_i, b_help - err_j
+                    if 0 < a[i] < self.c:
+                        b = b1
+                    elif 0 < a[j] < self.c:
+                        b = b2
+                    else:
+                        b = (b1 + b2) / 2
+                    num_changes_alpha += 1
+    
+            if num_changes_alpha == 0:
+                passes += 1
+            else:
+                passes = 0
+        for p in pts:
+            p.class_id = 0 if p.class_id == -1 else 1
 
-        if num_changes_alpha == 0:
-            passes += 1
+        return a, b
+    
+    def calc_n(self, x_i, x_j):
+        return 2 * self.kernel(x_i, x_j) - self.kernel(x_i, x_i) - self.kernel(x_j, x_j)
+    
+    def calc_e(self, x, a, b, pts):
+        return self.calc_f(x, a, b, pts) - x.class_id
+    
+    def calc_lh(self, i, j, pts, a):
+        if pts[i].class_id != pts[j].class_id:
+            return max(0, a[j] - a[i]), min(self.c, self.c + a[j] - a[i])
         else:
-            passes = 0
-    return a, b
-
-
-def calc_n(x_i, x_j, kernel):
-    return 2 * kernel(x_i, x_j) - kernel(x_i, x_i) - kernel(x_j, x_j)
-
-
-def calc_e(x, a, b, pts, kernel):
-    return calc_f(x, a, b, pts, kernel) - x.class_id
-
-
-def calc_lh(i, j, pts, a, c):
-    if pts[i].class_id != pts[j].class_id:
-        return max(0, a[j] - a[i]), min(c, c + a[j] - a[i])
-    else:
-        return max(0, a[i] + a[j] - c), min(c, a[i] + a[j])
-
-
-def calc_f(x, a, b, pts, kernel):
-    res = b
-    for i in range(len(a)):
-        res += a[i] * pts[i].class_id * kernel(x, pts[i])
-    return res
-
-
+            return max(0, a[i] + a[j] - self.c), min(self.c, a[i] + a[j])
+    
+    def calc_f(self, x, a, b, pts):
+        res = b
+        for i in range(len(a)):
+            res += a[i] * pts[i].class_id * self.kernel(x, pts[i])
+        return res
