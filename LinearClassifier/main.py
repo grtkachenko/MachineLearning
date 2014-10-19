f2efb93ea0905c85c0144a06ed99c1ecc2db3b94
Anton Kovsharov <antonkovsharov@gmail.com>
2014-10-19 22:05:10 +0400

    smo interface added

--- LinearClassifier/main.py
+++ LinearClassifier/main.py
@@ -1,6 +1,11 @@
 from common.point import Point
 from common.utils import normalize
+from smo import smo
 
-with open('LinearDataset', 'f') as f:
+data = []
+
+with open('LinearDataset', 'r') as f:
     data = [Point(*(line.split(' '))) for line in f.readlines()]
     data = normalize(normalize(data, 0), 1)
+
+a, b = smo(1, 1, 1, data)
---
+++ LinearClassifier/smo.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def smo(C, tol, max_passes, pts):
+    a = np.array(len(pts))
+    b = 0
+    return a, b
